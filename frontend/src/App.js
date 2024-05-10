58195a4a2f8f03ce97099c79b6333b3124317d73
Agafina <98034773+Agafina@users.noreply.github.com>
2024-05-10 03:50:57 +0100

    completed the header section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,7 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart"
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder"
 import Navbar from "./components/Navbar/Navbar";
 
 
@@ -5,6 +9,11 @@ function App() {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/cart" element={<Cart />}/>
+        <Route path="/order" element={<PlaceOrder />}/>
+      </Routes>
     </div>
   );
 }
