52fae3e070015d23490d303b213f1d78c1df72ba
Agafina <98034773+Agafina@users.noreply.github.com>
2024-05-11 12:09:30 +0100

    completed the add to cart state methods

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
+const ExploreMenu = ({ category, setCategory }) => {
+  const handleClick = (item) => {
+    setCategory((prev) => (prev === item.menu_name ? "All" : item.menu_name));
+  };
+  console.log(category);
+  return (
+    <div className="explore-menu">
+      <h1>Explore Our Menu</h1>
+      <p className="explore-menu-text">
+        Choose from a diverse menu featuring arrays of dishes. Our mission is to
+        Satisfy your cravings and elevate your dining experience, one delicious
+        meal at a time.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => (
+          <div
+            onClick={() => handleClick(item)}
+            className="explore-menu-list-item"
+            key={index}
+          >
+            <img
+              className={category === item.menu_name ? "active" : ""}
+              src={item.menu_image}
+              alt=""
+            />
+            <p>{item.menu_name}</p>
+          </div>
+        ))}
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default ExploreMenu;
---
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import "./FoodDisplay.css";
+import { StoreContext } from "../../context/StoreContext";
+import FoodItems from "../FoodItems/FoodItems";
+
+const FoodDisplay = () => {
+  const { food_list } = useContext(StoreContext);
+  return (
+    <div className="food-display" id="food-display">
+      <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          return (
+            <FoodItems
+              key={index}
+              name={item.name}
+              id={item._id}
+              price={item.price}
+              description={item.description}
+              image={item.image}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default FoodDisplay;
